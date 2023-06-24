170cacce26f7c685b9991af2c73f900fe2b7ab4c
BhagyaParkhi <137601024+BhagyaParkhi@users.noreply.github.com>
2023-06-24 12:48:30 +0530

    Add files via upload
    Api

---
+++ MyController.java
@@ -0,0 +1,17 @@
+package com.first1.API1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+@RequestMapping("name")
+	String firstApi()
+	{
+		return "Bhagyashri";
+	}
+void 
+
+
+}
